de7774526200d77b1543616de2044f2c8eeaa7be
DemetreKhosro <demetrexosroshvili@gmail.com>
2026-02-05 19:11:51 +0400

    second commit

--- level 204/homework/hw3.py
+++ level 204/homework/hw3.py
@@ -14,8 +14,8 @@ student1_sum = student1.math + student1.english + student1.science
 student2_sum = student2.math + student2.english + student2.science
 
 if student1_sum > student2_sum:
-  print(f"{student1.name}s score is higher than {student2.name} score")
+  print(f"{student1.student_name}s score is higher than {student2.student_name} score")
 elif student1_sum == student2_sum:
   print('both students scores are equal')
 else:
-  print(f"{student2.name}s score is higher than {student1.name} score")
+  print(f"{student2.student_name}s score is higher than {student1.student_name} score")
--- level 204/homework/hw5.py
+++ level 204/homework/hw5.py
@@ -1,27 +1,27 @@
 # 5) შექმენი კლასი Subscription: რომელსაც ექნება user_name, plan("free", "basic", "pro"), months_active. წესები: free -> max 1 month, basic -> max 6 months, pro -> unlimited. დაბეჭდე რომელ მომხმარებლებს აქვთ ვალიდური subscription. ლოგიკა დაწერე class-ის გარეთ
 
 class Subscription:
-    def __init__(self, user_name, plan, months_active):
-        self.user_name = user_name
-        self.plan = plan
-        self.months_active = months_active
+  def __init__(self, user_name, plan, months_active):
+    self.user_name = user_name
+    self.plan = plan
+    self.months_active = months_active
 
 subscriptions = [
-    Subscription("gio", "free", 1),
-    Subscription("ani", "free", 2),
-    Subscription("luka", "basic", 4),
-    Subscription("saba", "basic", 7),
-    Subscription("toma", "pro", 12)
+  Subscription("gio", "free", 1),
+  Subscription("ani", "free", 2),
+  Subscription("luka", "basic", 4),
+  Subscription("saba", "basic", 7),
+  Subscription("toma", "pro", 12)
 ]
 
 for sub in subscriptions:
-    is_valid = False
-    if sub.plan == "free" and sub.months_active <= 1:
-        is_valid = True
-    elif sub.plan == "basic" and sub.months_active <= 6:
-        is_valid = True
-    elif sub.plan == "pro":
-        is_valid = True
+  is_valid = False
+  if sub.plan == "free" and sub.months_active <= 1:
+    is_valid = True
+  elif sub.plan == "basic" and sub.months_active <= 6:
+    is_valid = True
+  elif sub.plan == "pro":
+    is_valid = True
 
-    if is_valid:
-        print(sub.user_name, sub.plan, sub.months_active)
+  if is_valid:
+    print(sub.user_name, sub.plan, sub.months_active)
